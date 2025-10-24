320b31522042fee4e94c71b099332bba58e1a9d2
Seth-Bullock <45029264+Seth-Bullock@users.noreply.github.com>
2025-10-24 14:23:21 +0100

    Update evlyrics_stream.py

--- evlyrics_stream.py
+++ evlyrics_stream.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import pandas as pd
 
+st.write("""
+# Evolving Lyrics
+""")
+
 from lyric_ga import do_the_ga
 
 do_the_ga()                         # this calls the Python code that runs the GA
 #exit()                              # this halts the execution of code in this file
 
 st.write("""
-# My second app
-Hello *world!*
+# Finished!
 """)
 
-df = pd.read_csv("my_data.csv")
+#df = pd.read_csv("my_data.csv")
 
-st.line_chart(df)
+#st.line_chart(df)
+
 
