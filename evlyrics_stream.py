f90c4dcbda33af986cf8e72febeed6c33670e766
Seth-Bullock <45029264+Seth-Bullock@users.noreply.github.com>
2025-10-24 14:26:00 +0100

    Update evlyrics_stream.py

--- evlyrics_stream.py
+++ evlyrics_stream.py
@@ -7,8 +7,12 @@ st.write("""
 
 from lyric_ga import do_the_ga
 
-do_the_ga()                         # this calls the Python code that runs the GA
+#do_the_ga()                         # this calls the Python code that runs the GA
 #exit()                              # this halts the execution of code in this file
+
+do_the_ga(author="taylor", target=None, length=25, elitism=True, crossover=0.5, pressure=3)
+#exit() # this exits the code - nothing below this line will be executed
+
 
 st.write("""
 # Finished!
@@ -17,5 +21,6 @@ st.write("""
 #df = pd.read_csv("my_data.csv")
 
 #st.line_chart(df)
+
 
 
