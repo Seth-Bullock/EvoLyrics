34e8c7df187774cc181f2b9ad16579e45cbb4064
Seth-Bullock <45029264+Seth-Bullock@users.noreply.github.com>
2025-10-24 14:20:24 +0100

    Update evlyrics_stream.py
    doing the ga

--- evlyrics_stream.py
+++ evlyrics_stream.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+from lyric_ga import do_the_ga
+
+do_the_ga()                         # this calls the Python code that runs the GA
+#exit()                              # this halts the execution of code in this file
+
 st.write("""
 # My second app
 Hello *world!*
@@ -9,3 +14,4 @@ Hello *world!*
 df = pd.read_csv("my_data.csv")
 
 st.line_chart(df)
+
